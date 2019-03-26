8b061182c6aff3beb5dcb2b5690b998500b2a788
user <user@DESKTOP-G8FHA3A>
2019-03-26 19:58:46 +0900

    gui filter

--- splc.apogames.analyzer/src/grapher/Grapher.java
+++ splc.apogames.analyzer/src/grapher/Grapher.java
@@ -22,8 +22,15 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Grapher extends JFrame implements MouseWheelListener, MouseListener{
+	private JFrame frame;
 	private Map<String, GraphNode> map = new HashMap<String, GraphNode>();
 	private List<GraphNode> nodes = new ArrayList<GraphNode>();
 	private List<GraphEdge> edges = new ArrayList<GraphEdge>();
@@ -35,9 +42,16 @@ public class Grapher extends JFrame implements MouseWheelListener, MouseListener
 	private Point startPoint;
 	private int x;
 	private int y;
+	private JTextField txtNodeRight;
+	private JTextField txtNodeLeft;
+	private JTextField txtEdgeRight;
+	private JTextField txtEdgeLeft;
 	
 	public Grapher() {
+		this.frame = this;
 		this.setSize(500, 500);
+		getContentPane().setLayout(null);
+		this.drawFilter();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		
@@ -48,10 +62,75 @@ public class Grapher extends JFrame implements MouseWheelListener, MouseListener
 		this.y = this.getY();
 	}
 	
+	public void drawFilter() {
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 162, 118);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblFliter = new JLabel("Fliter");
+		lblFliter.setBackground(Color.WHITE);
+		lblFliter.setHorizontalAlignment(SwingConstants.CENTER);
+		lblFliter.setBounds(35, 10, 84, 15);
+		panel.add(lblFliter);
+		
+		JLabel lblNode = new JLabel("\u2264 Node \u2264 ");
+		lblNode.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNode.setBounds(43, 35, 76, 15);
+		panel.add(lblNode);
+
+		txtNodeLeft = new JTextField();
+		txtNodeLeft.setBounds(12, 32, 35, 21);
+		txtNodeLeft.setColumns(10);
+		txtNodeLeft.setText("0.0");
+		panel.add(txtNodeLeft);
+		
+		txtNodeRight = new JTextField();
+		txtNodeRight.setBounds(115, 32, 35, 21);
+		txtNodeRight.setColumns(10);
+		txtNodeRight.setText("1.0");
+		panel.add(txtNodeRight);
+		
+		JLabel lblEdge = new JLabel("\u2264 Edge \u2264 ");
+		lblEdge.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEdge.setBounds(43, 63, 76, 15);
+		panel.add(lblEdge);
+		
+		txtEdgeLeft = new JTextField();
+		txtEdgeLeft.setBounds(12, 60, 35, 21);
+		txtEdgeLeft.setColumns(10);
+		txtEdgeLeft.setText("0.0");
+		panel.add(txtEdgeLeft);
+
+		txtEdgeRight = new JTextField();
+		txtEdgeRight.setBounds(115, 60, 35, 21);
+		txtEdgeRight.setColumns(10);
+		txtEdgeRight.setText("1.0");
+		panel.add(txtEdgeRight);
+		
+		JButton btnApply = new JButton("Apply");
+		btnApply.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				minNodeVal = Double.valueOf(txtNodeLeft.getText());
+				maxNodeVal = Double.valueOf(txtNodeRight.getText());
+				minEdgeWgt = Double.valueOf(txtEdgeLeft.getText());
+				maxEdgeWgt = Double.valueOf(txtEdgeRight.getText());
+				frame.repaint();
+			}
+		});
+		btnApply.setFont(new Font("±¼¸²", Font.PLAIN, 12));
+		btnApply.setBounds(43, 88, 76, 23);
+		panel.add(btnApply);
+		
+		JButton background = new JButton("");
+		background.setEnabled(false);
+		background.setBounds(0, 0, 162, 118);
+		panel.add(background);
+	}
+	
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		
 		g.translate(x, y);
 		
 		List<GraphNode> invisibleNodes = new ArrayList<GraphNode>();
